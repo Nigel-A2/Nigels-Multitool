33ef7fd103beba7635d390446b84e400c65ef875
NigelOOSD <nigel.awrey@edu.sait.ca>
2023-03-23 13:01:25 -0600

    Added more sound buttons
    Added a link to the homepage on the soundboard page. Added a link to my portfolio and the soundboard to the homepage

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Nigels-Multitool
-Multiple different tools useful for everyday life all in one easy-to-use website
+# Nigel's Multitool
+Multiple different tools useful for everyday life all in one easy-to-use website  
+
+
+[My portfolio](https://nigeloosd.github.io/)  
+
+[Soundboard](https://nigeloosd.github.io/Nigels-Multitool/soundboard.html)  
+More tools coming soon!
--- js/soundeffects.js
+++ js/soundeffects.js
@@ -3,3 +3,51 @@ PlayBoom = function () {
     audio.loop = false;
     audio.play(); 
 }
+
+PlayBell = function () {
+    var audio = new Audio('./sound-files/bell.mp3');
+    audio.loop = false;
+    audio.play(); 
+}
+
+PlayBeRightBack = function () {
+    var audio = new Audio('./sound-files/be-right-back.mp3');
+    audio.loop = false;
+    audio.play(); 
+}
+
+PlayDrumRoll = function () {
+    var audio = new Audio('./sound-files/ba-dum-tss.mp3');
+    audio.loop = false;
+    audio.play(); 
+}
+
+PlayDangerAlarm = function () {
+    var audio = new Audio('./sound-files/danger-alarm.mp3');
+    audio.loop = false;
+    audio.play(); 
+}
+
+PlayDunDun = function () {
+    var audio = new Audio('./sound-files/dun-dun.mp3');
+    audio.loop = false;
+    audio.play(); 
+}
+
+PlayGuitarRiff = function () {
+    var audio = new Audio('./sound-files/guitar-riff.mp3');
+    audio.loop = false;
+    audio.play(); 
+}
+
+PlayTruckHorn = function () {
+    var audio = new Audio('./sound-files/truck-horn.mp3');
+    audio.loop = false;
+    audio.play(); 
+}
+
+PlayWaterphone = function () {
+    var audio = new Audio('./sound-files/waterphone.mp3');
+    audio.loop = false;
+    audio.play(); 
+}
