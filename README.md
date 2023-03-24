48a1ac24c1351182d1be4c0307b3db557ff1b454
NigelOOSD <nigel.awrey@edu.sait.ca>
2023-03-23 19:54:25 -0600

    Added a length unit converter to the multitool

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Multiple different tools useful for everyday life all in one easy-to-use website
 
 [My portfolio](https://nigeloosd.github.io/)  
 
-[Soundboard](https://nigeloosd.github.io/Nigels-Multitool/soundboard.html)  
+[Soundboard](https://nigeloosd.github.io/Nigels-Multitool/soundboard.html)   
+[Length converter](https://nigeloosd.github.io/Nigels-Multitool/length-converter.html)
+
 More tools coming soon!
---
+++ js/length-converter.js
@@ -0,0 +1,23 @@
+function convert() {
+	// Get input values
+	var input_value = document.getElementById("input_value").value;
+	var from_unit = document.getElementById("from_unit").value;
+	var to_unit = document.getElementById("to_unit").value;
+
+	// Convert units
+	var units = {
+		m: 1,
+		km: 1000,
+		cm: 0.01,
+		mm: 0.001,
+		mi: 1609.34,
+		yd: 0.9144,
+		ft: 0.3048,
+		in: 0.0254
+	};
+
+	var result = (input_value * units[from_unit]) / units[to_unit];
+
+	// Display result to the nearest hundredth
+	document.getElementById("result").innerHTML = input_value + " " + from_unit + " = " + result.toFixed(2) + " " + to_unit;
+}
