67ff527d5b31bcea12c43b00e47f241abbd06073
NigelOOSD <nigel.awrey@edu.sait.ca>
2023-03-26 15:30:01 -0600

    Added more units to the length converter

--- js/length-converter.js
+++ js/length-converter.js
@@ -6,15 +6,26 @@ function convert() {
 
 	// Values of each unit. Meters are the default
 	var units = {
+        // metric units
 		m: 1,
 		km: 1000,
 		cm: 0.01,
 		mm: 0.001,
+        hm: 100,
+        dam: 10,
+        dm: 0.1,
+        μm: 0.000001,
+        // imperial units
 		mi: 1609.344,
 		yd: 0.9144,
 		ft: 0.3048,
 		in: 0.0254,
-        nmi: 1852
+        nmi: 1852,
+        rod: 5.0292,
+        chain: 20.1168,
+        link: 0.201168,
+        thou: 0.0000254,
+        league: 4828.032
 	};
     // Convert units
 	var result = (input_value * units[from_unit]) / units[to_unit];
